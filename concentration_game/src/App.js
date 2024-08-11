c588ff608b1ef9d005f0ca29db18c60a3206ebbd
Vulcan-Fire <121341189+Vulcan-Fire@users.noreply.github.com>
2024-08-12 02:42:12 +0530

    Update App.js

--- concentration_game/src/App.js
+++ concentration_game/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Instructions from "./components/Instructions";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/instructions" element={<Instructions />} />
         </Routes>
       </div>
     </Router>
