dd3abe09d5821e465292b1a1f087a22281746f89
Anitejjain <131655465+Anitejjain@users.noreply.github.com>
2024-08-12 01:23:39 +0530

    Create Home.js
    Created the Landing Page for the Game with Heading and Start Game Link

---
+++ concentration_game/src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="HomeContainer">
+      <header className="HomeHeader">
+        <h1 className="Welcome">
+          Welcome to Mind Grid!
+        </h1>
+        <p className="Tagline">
+          Challenge Your Memory, Conquer the Grid
+        </p>
+        <Link to="/game" className="StartLink">
+          Start Game
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
