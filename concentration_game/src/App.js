e3364f41f4d8e8005b1050cda8d52614249536d3
Vulcan-Fire <121341189+Vulcan-Fire@users.noreply.github.com>
2024-08-12 23:06:20 +0530

    Update App.js
    Imported audio file.

--- concentration_game/src/App.js
+++ concentration_game/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Instructions from "./components/Instructions";
 import Game from "./components/Game";
+import AudioPlayer from "./components/AudioPlayer";
+import backgroundMusic from "./assets/background-music.mp3";
 
 function App() {
   return (
     <Router>
       <div>
+        <AudioPlayer src={backgroundMusic} volume={0.3} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/instructions" element={<Instructions />} />
