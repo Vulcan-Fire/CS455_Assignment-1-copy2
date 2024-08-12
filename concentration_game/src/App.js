7ae8ca109ca3abb9727d98497c58ea9e67a53962
Anitejjain <131655465+Anitejjain@users.noreply.github.com>
2024-08-12 13:43:21 +0530

    Update App.js
    Added Game Route

--- concentration_game/src/App.js
+++ concentration_game/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Instructions from "./components/Instructions";
+import Grid from "./components/Grid";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/instructions" element={<Instructions />} />
+          <Route path="/game" element={<Grid />} />
         </Routes>
       </div>
     </Router>
