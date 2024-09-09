09065f17ecf1f0970121449f3053daee00aac6db
Manan Kumar <121341189+Vulcan-Fire@users.noreply.github.com>
2024-09-09 08:42:11 +0530

    Update Util.js

--- concentration_game/src/components/Util.js
+++ concentration_game/src/components/Util.js
@@ -1,11 +1,24 @@
-export const generateRandomBlocks = (totalBlocks, numDifferent) => {
+const generateUniqueRandomIndices = (totalBlocks, numDifferent) => {
   const differentIndices = new Set();
   while (differentIndices.size < numDifferent) {
     differentIndices.add(Math.floor(Math.random() * totalBlocks));
   }
-  return Array.from({ length: totalBlocks }, (_, index) =>
-    differentIndices.has(index)
-      ? { color: "#75ab00", isDifferent: true }
-      : { color: "#ffffff", isDifferent: false }
+  return differentIndices;
+};
+
+const createBlock = (index, differentIndices) => {
+  return differentIndices.has(index)
+    ? { color: "#75ab00", isDifferent: true }
+    : { color: "#ffffff", isDifferent: false };
+};
+
+const generateBlocksArray = (totalBlocks, differentIndices) => {
+  return Array.from({ length: totalBlocks }, (_, index) => 
+    createBlock(index, differentIndices)
   );
+};
+
+export const generateRandomBlocks = (totalBlocks, numDifferent) => {
+  const differentIndices = generateUniqueRandomIndices(totalBlocks, numDifferent);
+  return generateBlocksArray(totalBlocks, differentIndices);
 };
