b3b89fc0011e93b10e47f846e9c3dae01ca69277
Manan Kumar <121341189+Vulcan-Fire@users.noreply.github.com>
2024-09-12 00:16:44 +0530

    Update Board.js
    Updated to remove lint errors

--- concentration_game/src/components/Board.js
+++ concentration_game/src/components/Board.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types"; 
 import "./Board.css";
 
 const Block = ({ block, index, isFlipped, isClickable, handleClick }) => {
@@ -18,6 +19,16 @@ const Block = ({ block, index, isFlipped, isClickable, handleClick }) => {
   );
 };
 
+Block.propTypes = {
+  block: PropTypes.shape({
+    color: PropTypes.string.isRequired,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+  isFlipped: PropTypes.bool.isRequired,
+  isClickable: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
+
 const GameBoardGrid = ({
   blocks,
   flippedBlocks,
@@ -30,8 +41,8 @@ const GameBoardGrid = ({
     <div
       className="game-board"
       style={{
-        gridTemplateColumns: `repeat(${gridSize}, 50px)`,
-        gridTemplateRows: `repeat(${gridSize}, 50px)`,
+        gridTemplateColumns: repeat(${gridSize}, 50px),
+        gridTemplateRows: repeat(${gridSize}, 50px),
       }}
     >
       {blocks.map((block, index) => (
@@ -46,6 +57,18 @@ const GameBoardGrid = ({
       ))}
     </div>
   );
+};
+
+GameBoardGrid.propTypes = {
+  blocks: PropTypes.arrayOf(
+    PropTypes.shape({
+      color: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  flippedBlocks: PropTypes.arrayOf(PropTypes.bool).isRequired,
+  isGameVisible: PropTypes.bool.isRequired,
+  isGameOver: PropTypes.bool.isRequired,
+  handleBlockClick: PropTypes.func.isRequired,
 };
 
 const GameBoard = ({
@@ -66,6 +89,18 @@ const GameBoard = ({
       />
     </div>
   );
+};
+
+GameBoard.propTypes = {
+  blocks: PropTypes.arrayOf(
+    PropTypes.shape({
+      color: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  flippedBlocks: PropTypes.arrayOf(PropTypes.bool).isRequired,
+  isGameVisible: PropTypes.bool.isRequired,
+  isGameOver: PropTypes.bool.isRequired,
+  handleBlockClick: PropTypes.func.isRequired,
 };
 
 export default GameBoard;
