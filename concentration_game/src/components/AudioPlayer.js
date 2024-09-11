ecb8c58a10098add973f0a0e7ece3e6bcf16d9cd
Anitejjain <131655465+Anitejjain@users.noreply.github.com>
2024-09-12 00:13:57 +0530

    Update AudioPlayer.js
    Removed errors shown by eslint

--- concentration_game/src/components/AudioPlayer.js
+++ concentration_game/src/components/AudioPlayer.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
 import "./AudioPlayer.css";
 
 const useAudioPlayer = (audioRef, volume, isPlaying) => {
@@ -22,13 +23,18 @@ const PlayPauseButton = ({ isPlaying, togglePlayPause }) => (
   </button>
 );
 
+PlayPauseButton.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  togglePlayPause: PropTypes.func.isRequired,
+};
+
 const HomeButton = () => (
   <a href="/" className="home-link">
     <button className="home-button">Home</button>
   </a>
 );
 
-const AudioPlayer = ({ src, volume = 0.5, loop = true }) => {
+const AudioPlayer = ({ src, volume, loop }) => {
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -40,9 +46,23 @@ const AudioPlayer = ({ src, volume = 0.5, loop = true }) => {
     <div className="audio-player-container">
       <audio ref={audioRef} src={src} loop={loop} />
       <HomeButton />
-      <PlayPauseButton isPlaying={isPlaying} togglePlayPause={togglePlayPause} />
+      <PlayPauseButton
+        isPlaying={isPlaying}
+        togglePlayPause={togglePlayPause}
+      />
     </div>
   );
+};
+
+AudioPlayer.propTypes = {
+  src: PropTypes.string.isRequired,
+  volume: PropTypes.number,
+  loop: PropTypes.bool,
+};
+
+AudioPlayer.defaultProps = {
+  volume: 0.5,
+  loop: true,
 };
 
 export default AudioPlayer;
