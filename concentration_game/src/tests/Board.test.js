26c8fb93e488c9777ab467f1e3f66a4b3d0dc344
Manan Kumar <121341189+Vulcan-Fire@users.noreply.github.com>
2024-09-12 10:33:48 +0530

    Update Board.test.js
    Corrected the file path

--- concentration_game/src/tests/Board.test.js
+++ concentration_game/src/tests/Board.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import GameBoard from './Board';
+import GameBoard from '../components/Board';
 
 describe('GameBoard Component', () => {
   const blocks = [
