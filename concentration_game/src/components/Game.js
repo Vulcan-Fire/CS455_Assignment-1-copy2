c821cced3604f99db9b416cd3d9b7f6ca36ec677
Anitejjain <131655465+Anitejjain@users.noreply.github.com>
2024-09-12 23:15:29 +0530

    Update Game.js

--- concentration_game/src/components/Game.js
+++ concentration_game/src/components/Game.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types"; // Import PropTypes
+import PropTypes from "prop-types"; 
 import GameBoard from "./Board";
 import useGameLogic from "./FlipLogic";
 import "./Game.css";
