9fd1bb24cb4871185e5643c37e14080f17066e28
Anitejjain <131655465+Anitejjain@users.noreply.github.com>
2024-09-12 23:23:47 +0530

    Add files via upload

---
+++ Progress_Report/Final Report/files/components_Util_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Thu, 12 Sep 2024 17:46:02 GMT","sloc":23,"lloc":17,"functions":5,"deliveredBugs":0.18,"difficulty":14.56,"maintainability":79.348,"lintErrors":1}]
---
+++ Progress_Report/Final Report/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Thu, 12 Sep 2024 17:46:02 GMT","total":{"sloc":344,"maintainability":664.686},"average":{"sloc":43,"maintainability":"83.09"}}]
