248bbd7e3cc580637093ce3156b138e698655a1e
Manan Kumar <121341189+Vulcan-Fire@users.noreply.github.com>
2024-10-06 14:12:58 +0530

    Create User.js

---
+++ concentration_game/server/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  maxScore: {
+    type: Number,
+    default: 0,
+  },
+  levels: [
+    {
+      level: Number,
+      totalTiles: Number,
+      correctTilesSelected: Number,
+    },
+  ],
+});
+
+UserSchema.methods.calculateTotalScore = function () {
+  return this.levels.reduce((sum, level) => sum + level.correctTilesSelected, 0);
+};
+
+UserSchema.methods.updateMaxScore = function () {
+  const currentScore = this.calculateTotalScore();
+  if (currentScore > this.maxScore) {
+    this.maxScore = currentScore;
+  }
+  return this.maxScore;
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
