48358d5211e45837df86b270307c998f503b7e81
Manan Kumar <121341189+Vulcan-Fire@users.noreply.github.com>
2024-10-10 11:27:50 +0530

    Update authRoutes.js

--- concentration_game/server/routes/authRoutes.js
+++ concentration_game/server/routes/authRoutes.js
@@ -32,4 +32,14 @@ router.post('/auth', async (req, res) => {
   }
 });
 
+router.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error logging out' });
+        }
+        res.status(200).json({ message: 'Logout successful' });
+    });
+});
+
+
 module.exports = router;
